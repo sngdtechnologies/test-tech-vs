53acc4e49ea6f3fe8e0fbf85784b2446935943b2
Gilles Descartes <sngdtechnologies@gmail.com>
2024-09-21 10:50:28 +0100

    seed 20 users to the database

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,12 +23,14 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $timezones = ['CET', 'CST', 'GMT+1'];
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            "timezone" => $timezones[array_rand($timezones)]
         ];
     }
 
