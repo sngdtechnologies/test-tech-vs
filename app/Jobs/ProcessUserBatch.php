8969c44367204af50bd4586d73f0829d9673d523
Gilles Descartes <sngdtechnologies@gmail.com>
2024-09-21 15:44:26 +0100

    Create a job to process user batches

---
+++ app/Jobs/ProcessUserBatch.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Log;
+
+class ProcessUserBatch implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new job instance.
+     * @Param $users
+     */
+    public function __construct($users)
+    {
+        $this->users = $users;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        foreach ($this->users as $user) {
+            // Simulation of API call to supplier
+            Log::info("[$user->id] firstname: {$user->name}, timezone: '{$user->timezone}'");
+        }
+    }
+}
